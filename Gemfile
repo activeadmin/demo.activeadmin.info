ab8e4ad904e294b52c0c415c29f233010c7b1295
Philippe Creux <pcreux@gmail.com>
2011-05-11 20:08:30 -0700

    Seed 38 products, 100 users and 300 orders

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'rails', '3.0.6'
 gem 'sqlite3-ruby', :require => 'sqlite3'
 gem 'nifty-generators'
 gem "activeadmin", :git => "git://github.com/gregbell/active_admin", :ref => "0b82af204b9fa1db369b7c5617564745cb94baa9"
+gem "faker"
 
 group :development do
   gem 'mechanize'
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,55 @@
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 #
 
+# Create default admin user
+AdminUser.create! do |a|
+  a.email = 'admin@example.com'
+  a.password = a.password_confirmation = 'password'
+end
+
+# Create default user
+User.create! do |u|
+  u.username = 'user'
+  u.email = 'user@example.com'
+  u.password = u.password_confirmation = 'password'
+end
 
 # Load each product from the yaml file
 YAML.load_file(File.expand_path("../seeds/products.yml", __FILE__)).each do |product|
   Product.create! product
+end
+
+NB_PRODUCTS = Product.count
+
+# Create 100 users
+NB_USERS = 100
+
+NB_USERS.times do |n|
+  User.create! do |u|
+    u.username = Faker::Internet.user_name + n.to_s
+    u.email = Faker::Internet.email.gsub('@', "#{n}@")
+    u.password = u.password_confirmation = 'password'
+  end
+end
+
+# Create 300 Orders
+NB_ORDERS = 300
+
+NB_ORDERS.times do
+  user_id = rand(NB_USERS - 1) + 1
+  user = User.find(user_id)
+  order = user.orders.create!
+  nb_items = rand(9) + 1
+
+  nb_items.times do
+    product_id = rand(NB_PRODUCTS - 1) + 1
+    product = Product.find(product_id)
+    LineItem.create! do |l|
+      l.order = order
+      l.product = product
+      l.price = product.price
+    end
+  end
+
+  order.recalculate_price! and order.checkout! if rand(100) < 90
 end
