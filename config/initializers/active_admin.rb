505ee109fcbb2670310eddfd0d0f2489f9238fea
Philippe Creux <pcreux@gmail.com>
2011-05-12 13:03:04 -0700

    Rename app to Active*A*dmin Depot

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -5,7 +5,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Activeadmin Depot"
+  config.site_title = "ActiveAdmin Depot"
 
 
   # == Default Namespace
