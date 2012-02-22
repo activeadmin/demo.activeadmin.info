c92dee23034dd138df4466e70e0b7090f742bd76
Fábio Luiz Nery de Miranda <fabio@miranti.net.br>
2012-02-22 13:41:02 -0200

    Add validation tests in unit/product_test.rb

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -16,5 +16,17 @@ class ProductTest < ActiveSupport::TestCase
   def test_should_be_valid
     assert new_product.valid?
   end
+
+  def test_require_title
+    assert new_product(:title => '').errors[:title]
+  end
+
+  def test_require_price
+    assert new_product(:price => '').errors[:price]
+  end
+
+  def test_require_image
+    assert new_product(:image_file_name => '').errors[:image_file_name]
+  end
 end
 
