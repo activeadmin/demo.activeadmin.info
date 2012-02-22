af1a88d895b4b7508d6f664abd6a4db9f58f7382
Fábio Luiz Nery de Miranda <fabio@miranti.net.br>
2012-02-22 13:35:15 -0200

    Fix unit/product_test.rb

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  def new_product(attrs = {})
+    attrs[:title]           ||= 'FooBar Product'
+    attrs[:price]           ||= 123.45
+    attrs[:image_file_name] ||= 'foobar.jpeg'
+    product = Product.new(attrs)
+    product
+  end
+
+  def setup
+    Product.delete_all
+  end
+
   def test_should_be_valid
-    assert Product.new.valid?
+    assert new_product.valid?
   end
 end
+
