aad4231e72be34eee5ce4f2da745a160e2a61bc8
Fábio Luiz Nery de Miranda <fabio@miranti.net.br>
2012-02-22 23:12:35 -0200

    Add user_address belongs_to user

--- app/admin/user_addresses.rb
+++ app/admin/user_addresses.rb
@@ -1,4 +1,18 @@
 ActiveAdmin.register UserAddress do
+  belongs_to  :user
 
+  form do |f|
+    f.inputs do
+      f.input :user_id,     :as => :hidden, :value => params[:user_id]
+      f.input :fullname
+      f.input :address_line1
+      f.input :address_line2
+      f.input :city
+      f.input :state
+      f.input :zipcode
+      f.input :country
+    end
+    f.buttons :commit
+  end
 end
 
