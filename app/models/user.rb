0d349b7e453f78a43b981bf9abd4a0b7581c929b
Philippe Creux <pcreux@gmail.com>
2014-12-27 15:10:03 -0800

    Fix email validation regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   validates_presence_of :username
   validates_uniqueness_of :username, :email, :allow_blank => true
   validates_format_of :username, :with => /(?=.*[A-Za-z._@])/, :allow_blank => true, :message => "should only contain letters, numbers, or .-_@"
-  validates_format_of :email, :with => /(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[\W])/
+  validates_format_of :email, :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
   validates_presence_of :password, :on => :create
   validates_confirmation_of :password
   validates_length_of :password, :minimum => 4, :allow_blank => true
