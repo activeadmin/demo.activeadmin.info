682be8b0088f84ccd765bd46043482749de73521
Igor Fedoronchuk <fedoronchuk@gmail.com>
2017-06-10 20:58:36 +0300

    update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,100 +10,94 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120222083938) do
+ActiveRecord::Schema.define(version: 20120222083938) do
 
-  create_table "active_admin_comments", force: true do |t|
-    t.string   "namespace"
-    t.text     "body"
-    t.string   "resource_id",   null: false
-    t.string   "resource_type", null: false
-    t.integer  "author_id"
-    t.string   "author_type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  create_table "active_admin_comments", force: :cascade do |t|
+    t.string "namespace"
+    t.text "body"
+    t.string "resource_type"
+    t.integer "resource_id"
+    t.string "author_type"
+    t.integer "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
+    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
+    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
   end
 
-  add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
-  add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace"
-  add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
-
-  create_table "admin_users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+  create_table "admin_users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admin_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
-  add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
-  add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
-
-  create_table "line_items", force: true do |t|
-    t.integer  "order_id"
-    t.integer  "product_id"
-    t.decimal  "price"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  create_table "line_items", force: :cascade do |t|
+    t.integer "order_id"
+    t.integer "product_id"
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_line_items_on_order_id"
+    t.index ["product_id"], name: "index_line_items_on_product_id"
   end
 
-  add_index "line_items", ["order_id"], name: "index_line_items_on_order_id"
-  add_index "line_items", ["product_id"], name: "index_line_items_on_product_id"
-
-  create_table "orders", force: true do |t|
-    t.integer  "user_id"
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id"
     t.datetime "checked_out_at"
-    t.decimal  "total_price",    precision: 8, scale: 2, default: 0.0
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.decimal "total_price", precision: 8, scale: 2, default: "0.0"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["checked_out_at"], name: "index_orders_on_checked_out_at"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
-  add_index "orders", ["checked_out_at"], name: "index_orders_on_checked_out_at"
-  add_index "orders", ["user_id"], name: "index_orders_on_user_id"
-
-  create_table "products", force: true do |t|
-    t.string   "title"
-    t.text     "description"
-    t.string   "author"
-    t.decimal  "price"
-    t.boolean  "featured"
-    t.date     "available_on"
-    t.string   "image_file_name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  create_table "products", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "author"
+    t.decimal "price"
+    t.boolean "featured"
+    t.date "available_on"
+    t.string "image_file_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["available_on"], name: "index_products_on_available_on"
+    t.index ["featured"], name: "index_products_on_featured"
   end
 
-  add_index "products", ["available_on"], name: "index_products_on_available_on"
-  add_index "products", ["featured"], name: "index_products_on_featured"
-
-  create_table "user_addresses", :force => true do |t|
-    t.integer  "user_id"
-    t.string   "fullname"
-    t.string   "address_line1"
-    t.string   "address_line2"
-    t.string   "city"
-    t.string   "state"
-    t.string   "zipcode"
-    t.string   "country"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  create_table "user_addresses", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "fullname"
+    t.string "address_line1"
+    t.string "address_line2"
+    t.string "city"
+    t.string "state"
+    t.string "zipcode"
+    t.string "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_addresses_on_user_id"
   end
 
-  add_index "user_addresses", ["user_id"], :name => "index_user_addresses_on_user_id"
-
-  create_table "users", force: true do |t|
-    t.string   "username"
-    t.string   "email"
-    t.string   "password_hash"
-    t.string   "password_salt"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_hash"
+    t.string "password_salt"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
