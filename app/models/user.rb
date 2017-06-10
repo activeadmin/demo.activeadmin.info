fc1234928107aa4c06a580455518feeaf07a960f
Piers Chambers <piers@varyonic.com>
2017-06-10 11:00:34 -0400

    Rubocop auto-correct.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :orders, :dependent => :destroy
+  has_many :orders, dependent: :destroy
 
   has_many :user_addresses
 
@@ -7,12 +7,14 @@ class User < ActiveRecord::Base
   before_save :prepare_password
 
   validates_presence_of :username
-  validates_uniqueness_of :username, :email, :allow_blank => true
-  validates_format_of :username, :with => /\A(?=.*[A-Za-z._@])\z/, :allow_blank => true, :message => "should only contain letters, numbers, or .-_@"
-  validates_format_of :email, :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
-  validates_presence_of :password, :on => :create
+  validates_uniqueness_of :username, :email, allow_blank: true
+  validates_format_of :username, with: /\A(?=.*[A-Za-z._@])\z/,
+                                 allow_blank: true,
+                                 message: "should only contain letters, numbers, or .-_@"
+  validates_format_of :email, with: /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
+  validates_presence_of :password, on: :create
   validates_confirmation_of :password
-  validates_length_of :password, :minimum => 4, :allow_blank => true
+  validates_length_of :password, minimum: 4, allow_blank: true
 
   # login can be either username or email address
   def self.authenticate(login, pass)
@@ -21,7 +23,7 @@ class User < ActiveRecord::Base
   end
 
   def matching_password?(pass)
-    self.password_hash == encrypt_password(pass)
+    password_hash == encrypt_password(pass)
   end
 
   private
