9867965d50ca9685c1e6946c9ef8317847d95701
David Rodríguez <deivid.rodriguez@riseup.net>
2021-02-03 22:14:44 +0100

    Move authentication module to main controller

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ Bundler.require(*Rails.groups)
 
 module ActiveadminDepot
   class Application < Rails::Application
-    config.autoload_paths << "#{config.root}/lib"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
