d9fdbbc66435cb8a1fa2546b22fb343531f99f06
David Rodríguez <deivid.rodriguez@riseup.net>
2021-03-11 09:42:29 +0100

    Fix other places to handle uploaded images

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,7 @@
 module ProductsHelper
+  def static_or_uploaded_image_tag(product, size, **options)
+    image_url = product.image.attached? ? product.image.variant(resize_and_pad: size) : "products/" + product.image_file_name
+
+    image_tag(image_url, **options)
+  end
 end
