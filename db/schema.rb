5606a6935b21172baee3d3eb85c736eac6027cc8
David Rodríguez <deivid.rodriguez@riseup.net>
2022-02-16 14:50:54 +0100

    Sync schema (#581)

--- db/schema.rb
+++ db/schema.rb
@@ -24,9 +24,9 @@ ActiveRecord::Schema.define(version: 2021_03_10_145103) do
     t.bigint "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
+    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author"
     t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
-    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
+    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource"
   end
 
   create_table "active_storage_attachments", force: :cascade do |t|
