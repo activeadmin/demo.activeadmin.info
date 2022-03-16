b464912bca47ae466dc419a368cf35e4d3ff3636
David Rodríguez <deivid.rodriguez@riseup.net>
2022-03-16 14:18:09 +0100

    Remove spring and listen (#619)
    Rails 7 does not ship these gems by default, let's get rid of them.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -8,8 +8,8 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.cache_classes = false
-  config.action_view.cache_template_loading = true
+  # Turn false under Spring and add config.action_view.cache_template_loading = true.
+  config.cache_classes = true
 
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
