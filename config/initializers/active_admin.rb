b697980fa03d2b26db461ad5cef0338ae15b3329
David Rodríguez <deivid.rodriguez@riseup.net>
2022-03-23 09:57:11 +0100

    Link back to front end from admin interface (#628)

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -9,7 +9,7 @@ ActiveAdmin.setup do |config|
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
   #
-  # config.site_title_link = "/"
+  config.site_title_link = "/"
 
   # Set an optional image to be displayed for the header
   # instead of a string (overrides :site_title)
