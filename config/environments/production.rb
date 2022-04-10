169bfeac048e31b130be03da4066aacf71396af7
David Rodríguez <deivid.rodriguez@riseup.net>
2022-04-10 18:40:23 +0200

    Configure `rack-throttle` again (#640)
    We used to have this configured, but I removed it accidentally.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.middleware.use Rack::Throttle::Daily,    :max => 1000  # requests
+  config.middleware.use Rack::Throttle::Minute,   :max => 60    # requests
 end
