56eedfc954081281413239ede7a14249b8d643b7
Piers Chambers <piers@varyonic.com>
2023-08-16 13:58:20 -0400

    Extract views/admin/users/_form.html.arb

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -17,15 +17,6 @@ ActiveAdmin.register User do
     actions
   end
 
-  form do |f|
-    f.inputs do
-      f.input :username
-      f.input :password, input_html: { autocomplete: "new-password" }
-      f.input :email
-    end
-    f.actions
-  end
-
   sidebar "Customer Details", only: :show do
     attributes_table_for user, :username, :email, :created_at
   end
--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -12,4 +12,20 @@ RSpec.describe User do
     expect(page).to have_css 'h2#page_title', text: user.username
     expect(page).to have_css 'h3', text: 'Order History'
   end
+
+  context 'new user' do
+    let(:user) { FactoryBot.build(:user) }
+
+    scenario 'create' do
+      visit admin_users_path
+      click_on 'New User'
+      expect(current_path).to eq new_admin_user_path(user)
+      expect(page).to have_css 'h2', text: 'New User'
+      fill_in 'Username', with: user.username
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+      click_on 'Create User'
+      expect(page).to have_css 'div.flash_notice', text: 'User was successfully created.'
+    end
+  end
 end
