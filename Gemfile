cd9193d7cbdfcfdc2c7dad1ae932078c1e5ea7da
Javier Julio <JJfutbol@gmail.com>
2024-01-06 12:46:05 -0500

    Add test suite with CI (#816)
    * Add initial system tests
    
    This covers current functionality. Asserting redirects isn't available in system tests so we'll need to create integration tests instead for that assertion.
    
    * Switch to headless_chrome
    
    This avoids bringing up the actual browser when running tests and should also just work in CI too.
    
    * Disable admin user fixtures
    
    These cause errors since they have database constraints and we'd need to fill these out. For now we'll just have a helper to create a default admin user.
    
    * Add simplecov gem setup
    
    * Add tests CI workflow
    
    * Fix Rails secrets deprecation from Devise
    
    Known issue and waiting on PR merge and new release. In the meantime, we can apply this simple fix.
    
    ```
    DEPRECATION WARNING: `Rails.application.secrets` is deprecated in favor of `Rails.application.credentials` and will be removed in Rails 7.2. (called from <top (required)> at /home/runner/work/demo.activeadmin.info/demo.activeadmin.info/config/environment.rb:5)
    ```
    
    https://github.com/heartcombo/devise/pull/5645#issuecomment-1871849856

--- Gemfile
+++ Gemfile
@@ -25,4 +25,6 @@ group :test do
   # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
   gem "capybara"
   gem "selenium-webdriver"
+  gem "simplecov", require: false
+  gem "simplecov-cobertura"
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,14 @@
+if ENV.fetch("COVERAGE", false)
+  require "simplecov"
+  require "simplecov-cobertura"
+  SimpleCov.start do
+    add_filter %r{^/test/}
+    minimum_coverage 98
+    maximum_coverage_drop 0.2
+    formatter SimpleCov::Formatter::CoberturaFormatter
+  end
+end
+
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
@@ -11,5 +22,14 @@ module ActiveSupport
     fixtures :all
 
     # Add more helper methods to be used by all tests here...
+    include Devise::Test::IntegrationHelpers
+
+    def default_admin_user
+      @default_admin_user ||= AdminUser.create!(
+        email: "admin@example.com",
+        password: "password",
+        password_confirmation: "password"
+      )
+    end
   end
 end
