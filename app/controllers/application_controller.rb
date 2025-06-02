0bf516d9e6f3d11c3e17a6675edb060c22727180
Geremia Taglialatela <tagliala@users.noreply.github.com>
2025-06-02 09:10:54 +0200

    Update app/controllers/application_controller.rb
    Co-authored-by: Javier Julio <JJfutbol@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  rate_limit to: 60, within: 1.minute
-  rate_limit to: 1000, within: 1.day
+  rate_limit to: 30, within: 1.minute
+  rate_limit to: 500, within: 1.day
 
   def route_not_found
     render file: Rails.public_path.join("404.html"), status: :not_found, layout: false
