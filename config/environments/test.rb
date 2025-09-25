8a781f2e489bac541d849d687113a241e49ebfc1
Javier Julio <jjfutbol@gmail.com>
2025-09-25 16:59:29 -0400

    Bump rails from 8.0.2.1 to 8.0.3

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # The test environment is used exclusively to run your application's
 # test suite. You never need to work with it otherwise. Remember that
 # your test database is "scratch space" for the test suite and is wiped
