d50024636227a821b32f9ee995ee27fa186f6f2e
Javier Julio <jjfutbol@gmail.com>
2025-09-25 17:15:37 -0400

    Add coverage for ransackable_* methods
    With this we now have 100% coverage.

--- test/models/admin_user_test.rb
+++ test/models/admin_user_test.rb
@@ -3,7 +3,11 @@
 require "test_helper"
 
 class AdminUserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "ransackable_attributes" do
+    assert AdminUser.ransackable_attributes
+  end
+
+  test "ransackable_associations" do
+    assert_empty AdminUser.ransackable_associations
+  end
 end
