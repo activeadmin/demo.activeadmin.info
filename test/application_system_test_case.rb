ed43a232258e24a9e049f29fe0eab1f49fed69e3
Javier Julio <jjfutbol@gmail.com>
2025-09-25 17:17:46 -0400

    Fix Devise mapping not found error in Rails 8
    This fixes a Devise mapping error on system tests.
    
    ```
    E
    
    Error:
    AdminUsersTest#test_visiting_the_edit:
    RuntimeError: Could not find a valid mapping for #<AdminUser id: 1, email: [FILTERED], created_at: "2025-09-25 21:07:53.971537000 +0000", updated_at: "2025-09-25 21:07:53.971537000 +0000">
        test/system/active_admin/admin_users_test.rb:42:in 'block in <class:AdminUsersTest>'
    
    bin/rails test test/system/active_admin/admin_users_test.rb:41
    ```

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -4,4 +4,10 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+
+  def setup
+    # Fix Devise mapping not found error in Rails 8 when running these spec types directly
+    # https://github.com/heartcombo/devise/issues/5774#issuecomment-2944728967
+    Rails.application.try(:reload_routes_unless_loaded)
+  end
 end
